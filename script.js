6446f5a778f2f2579b8347150f6a5951c65e8b14
Шмелев Иван <ishmelev@1ditis.ru>
2019-04-23 15:22:08 +0300

    23.04

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const obj = {
   four: {
     fourOne: 1,
     fourTwo: {
-      fourTwoOne: 1
+      fourTwoOne: 1,
     }
+  },
+  five: [23, [123, 1]],
+  fiveOne: {
+    fiveOneOne:1
   }
 }
-
-// document.getElementsByTagName('body')
 
 let getTree = (tree) => {
   let paths = [];
@@ -33,34 +35,57 @@ let getTree = (tree) => {
 
 let buildStructure = (tree) => {
   let branch = [];
-  let mainElement;
   for(let i in tree){
     branch.push(tree[i].split('>'));
   }
 
-  for(let i in tree){
-    mainElement = document.createElement('div');
-    document.body.appendChild(mainElement);
-    mainElement.innerText = tree[i];
-
-    for(let j in branch[i]){
-      let element = document.createElement('div');
-      if(j == 0){
-        mainElement.appendChild(element);
-      }
-      else{
-        let underElement = document.createElement('div');
-        element.appendChild(underElement);
-      }
+  let showInfo = (e) => {
+    let branch = e.target.classList[0];
+    branch = Array.from(branch).pop();
+    console.log(branch);
+    if(typeof block == 'undefined'){
+      block = document.createElement('div');
     }
+
+    let strOfInfo = ``;
+
+    for(let i in arr = getTree(obj)){
+      if(i == branch){
+        strOfInfo = arr[i];
+      }
+      block.innerHTML = `<div>${strOfInfo}</div>`;
+      
+      document.body.appendChild(block);
+    }
+    block.className = "info-block";
+    block.style.top = e.clientY + "px";
+    block.style.left = window.innerWidth - (window.innerWidth * 0.7) + "px";
   }
 
-  
+  for(let i in branch){
+    for(let j in branch[i]){
+      let element = document.querySelectorAll('.branch-'+i);
+      let newElement = document.createElement('div');
+      newElement.className = "branch-"+i;
+      newElement.dataset.objValue = branch[i][j];
+      newElement.innerText = branch[i][j];
+      newElement.title = `This element of object with value - ${branch[i][j]}`;
+      newElement.addEventListener("mousemove", showInfo);
+      if(element.length > 0){
+        newElement.className = newElement.className + " element";
+        element[element.length-1].appendChild(newElement);
+      }
+      else{
+        newElement.className = newElement.className + " main-element";
+        document.body.appendChild(newElement);
+      }
+    }
+    element = [];
+  }
 }
 
 window.onload = () => {
   buildStructure(getTree(obj));
-  console.log(getTree(obj));
 }
 
 
